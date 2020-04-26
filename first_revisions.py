cdb69f9ed7d112f139e68168b5b4065201ee674f
Christopher Sampah <christopher.m.sampah@gmail.com>
2020-04-26 11:36:47 -0400

    Made the revision script the real generate_trid_card script, and moved the old generate_trid_card script to the graveyard

--- first_revisions.py
+++ first_revisions.py
@@ -10,7 +10,7 @@ from mailmerge import MailMerge
 import pandas as pd
 import numpy as np
 import os
-from agent_license_automation.generation_functions import fix, pre_process, pad_the_df, template_fields, split_the_data
+from agent_license_automation.generation_functions import fix, pre_process, pad_the_df, split_the_data
 
 def format_to_print(df):
     for col in ['_1', '_2', '_3']:
@@ -20,7 +20,7 @@ def format_to_print(df):
 
     for col in ['_1', '_2', '_3']:
         #Precede the chars "License #:" before the actual license number value
-        df.loc[df['license_number'+col].str.len() >3,['state'+col]] = df['state'+col] + ' License #: '
+        df.loc[df['license_number'+col].str.len() >3,['state'+col]] = df['state'+col] + ' Firm License #: '
         #If a license number doesnt have an expiration date, add "missing" to the expiration date"
         df.loc[(df['license_number'+col].str.len() > 3) & (df['expiration_date'+col].isna()), ['expiration_date'+col]] = 'missing'
         #If an agent is licensed in 3 states, precede the expiration date value with "Exp Date:"
@@ -40,6 +40,11 @@ def format_to_print(df):
         one_license[col+'1'] = 'nan'
     df = pd.concat([df, one_license])
     
+    'based on what office they work in, print that state on the back of the TRID card'
+    df['office_state'] = 'VA'
+    df.loc[df['office'] == 'rockville',['office_state']] = 'MD'
+    df['office_state'] = df['office_state'] + ' Firm License #'
+    
     'remove nan so that the final output doesnt have "nan" in it'
     df.replace(np.nan, '', inplace = True)
     df.replace('nan', '', inplace = True)
@@ -48,10 +53,11 @@ def format_to_print(df):
     df.sort_values(by = 'last_name', inplace = True)
     return df
 
-def make_trid_cards(df, output_dir, template = 'new template.docx'):
+def make_trid_cards(df, output_dir, template = 'new template.docx', doc_name = None):
     print('Creating the page of contact cards...')
     
     document = MailMerge(template)
+    #print(document.get_merge_fields())
     
     df = pad_the_df(df)
     box_1 = df.iloc[0]
@@ -71,6 +77,7 @@ def make_trid_cards(df, output_dir, template = 'new template.docx'):
     document.merge(
         
         #box 1
+        office_state_1 = box_1['office_state'],
         first_1 = box_1['first_name'],
         last_1 = box_1['last_name'],
         
@@ -87,6 +94,7 @@ def make_trid_cards(df, output_dir, template = 'new template.docx'):
         exp_date_1_3 = box_1['expiration_date_3'],
     
         #box 2
+        office_state_2 = box_1['office_state'],
         first_2 = box_2['first_name'],
         last_2 = box_2['last_name'],
         
@@ -103,6 +111,7 @@ def make_trid_cards(df, output_dir, template = 'new template.docx'):
         exp_date_2_3 = box_2['expiration_date_3'],
     
         #box 3
+        office_state_3 = box_3['office_state'],
         first_3 = box_3['first_name'],
         last_3 = box_3['last_name'],
         
@@ -119,6 +128,7 @@ def make_trid_cards(df, output_dir, template = 'new template.docx'):
         exp_date_3_3 = box_3['expiration_date_3'],
     
         #box 4
+        office_state_4 = box_4['office_state'],
         first_4 = box_4['first_name'],
         last_4 = box_4['last_name'],
         
@@ -135,6 +145,7 @@ def make_trid_cards(df, output_dir, template = 'new template.docx'):
         exp_date_4_3 = box_4['expiration_date_3'],
         
         #box 5
+        office_state_5 = box_5['office_state'],
         first_5 = box_5['first_name'],
         last_5 = box_5['last_name'],
         
@@ -151,6 +162,7 @@ def make_trid_cards(df, output_dir, template = 'new template.docx'):
         exp_date_5_3 = box_5['expiration_date_3'],
         
         #box 6
+        office_state_6 = box_6['office_state'],
         first_6 = box_6['first_name'],
         last_6 = box_6['last_name'],
         
@@ -167,6 +179,7 @@ def make_trid_cards(df, output_dir, template = 'new template.docx'):
         exp_date_6_3 = box_6['expiration_date_3'],
         
         #box 7
+        office_state_7 = box_7['office_state'],
         first_7 = box_7['first_name'],
         last_7 = box_7['last_name'],
         
@@ -183,6 +196,7 @@ def make_trid_cards(df, output_dir, template = 'new template.docx'):
         exp_date_7_3 = box_7['expiration_date_3'],
         
         #box 8
+        office_state_8 = box_8['office_state'],
         first_8 = box_8['first_name'],
         last_8 = box_8['last_name'],
         
@@ -199,6 +213,7 @@ def make_trid_cards(df, output_dir, template = 'new template.docx'):
         exp_date_8_3 = box_8['expiration_date_3'],
         
         #box 9
+        office_state_9 = box_9['office_state'],
         first_9 = box_9['first_name'],
         last_9 = box_9['last_name'],
         
@@ -215,6 +230,7 @@ def make_trid_cards(df, output_dir, template = 'new template.docx'):
         exp_date_9_3 = box_9['expiration_date_3'],
         
         #box 10
+        office_state_10 = box_10['office_state'],
         first_10 = box_10['first_name'],
         last_10 = box_10['last_name'],
         
@@ -231,6 +247,7 @@ def make_trid_cards(df, output_dir, template = 'new template.docx'):
         exp_date_10_3 = box_10['expiration_date_3'],
         
         #box 11
+        office_state_11 = box_11['office_state'],
         first_11 = box_11['first_name'],
         last_11 = box_11['last_name'],
         
@@ -247,6 +264,7 @@ def make_trid_cards(df, output_dir, template = 'new template.docx'):
         exp_date_11_3 = box_11['expiration_date_3'],
         
         #box 12
+        office_state_12 = box_12['office_state'],
         first_12 = box_12['first_name'],
         last_12 = box_12['last_name'],
         
@@ -264,15 +282,23 @@ def make_trid_cards(df, output_dir, template = 'new template.docx'):
     
     n = df.shape[0]            
     file_name = df['last_name'].iloc[0] + '_to_' + df['last_name'].iloc[n-1]
+    if doc_name is not None:
+        if type(doc_name) is str: file_name = doc_name
     document.write(output_dir + '/' +file_name + '.docx')
     print('Done')
     
 if __name__ == "__main__":
     df = format_to_print(pre_process(pd.read_csv("all_agent_info.csv")))
+    
     dfs = split_the_data(df)
-    [make_trid_cards(df, template = 'new template.docx', output_dir = 'output') for df in dfs]
-        
-        
+    
+    path = '//192.168.50.245/Staff/2018/CSR Front Desk/MCL-RV-ARL Shared Folder/TRID Cards 2020'
+    [make_trid_cards(df, template = 'new template.docx', output_dir = path) for df in dfs]
+    
+    sample_df = pd.concat([df.loc[df['office'] == 'mclean'].head(9),
+                           df.loc[df['office'] == 'rockville'].head(3)])
+    
+    make_trid_cards(sample_df, template = 'new template.docx',output_dir = path, doc_name = '0-sample for Suzzette')        
         
         
         
